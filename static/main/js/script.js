ebcc1509a2c5462d1a516acf9a80c011fe0b0947
Chris van den Eijnden <chrisvdeijnden@gmail.com>
2026-01-20 09:22:03 +0100

    chore: added show/hide button

--- static/main/js/script.js
+++ static/main/js/script.js
@@ -29,3 +29,15 @@ document.addEventListener('DOMContentLoaded', function() {
 function handleGoogleLogin() {
     window.location.href = "{% url 'social:begin' 'google-oauth2' %}";
 }
+
+function togglePassword(inputId, button) {
+    const input = document.getElementById(inputId);
+    
+    if (input.type === 'password') {
+        input.type = 'text';
+        button.textContent = 'HIDE';
+    } else {
+        input.type = 'password';
+        button.textContent = 'SHOW';
+    }
+}
